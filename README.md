7bccfab9b607c44aeb6e4bd14eb65a4eb62b775c
Daniel Skowroński <daniel@dsinf.net>
2016-09-12 19:47:27 +0200

    first version with MVP example

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # sflider
 as simple as possible jQuery based slider with mavigation
+
+### goals
+- many automagic and minimum config
+- simple transitions only (lightweight)
+- content of slide cany be any html div
+
+### features
+- changing slides every configured time period
+- navigation buttons
+- pause changing when hovered (including manual navigation via buttons)
+
+### usage
+1. include **sflider.css** and **sflider.js**
+2. create JS object with at least two fields:
+   - **height** (taking any CSS-friendly format) and
+   - **timeout** - number of milliseconds between slides changes
+3. after DOM is created (jQuery [ready event](https://api.jquery.com/ready/)) execute **_invokeSflider** functiuon with 2 params:
+   - first - config object
+   - second - jQeury object of slider container
+4. slider container needs to have at least **.sflider-slides** div containing one or more **.sflider-slide** divs with any content
+5. to change slide execute **_moveSflider** with params:
+    - **offset** - any number (negative changes slides backwards)
+    - *optional **force*** - should hover over object be ignored (eg. clicking button that is located on main slider div)
+
+refer to **examples/** directory
---
+++ sflider.js
@@ -0,0 +1,72 @@
+/* sflider */
+
+_sfliderState = {
+  currentSlideId: 0,
+  slideChangeAllowed: true,
+  config: null,
+  obj: null,
+  slidesCount: 0
+}
+
+function fixHeight(obj, value){
+  $(obj).css("height", value);
+  $(obj).css("max-height", value);
+}
+function mod(n, m) {
+  return ((n % m) + m) % m; /* why would JS has modulo working properly with negative numbers? */
+}
+function setSfliderTimer(){
+  clearTimeout(_sfliderState.sliderTimer);
+  _sfliderState.sliderTimer = setTimeout( "_moveSflider(+1)", _sfliderState.config.timeout );
+}
+
+function _invokeSflider(config, obj){
+  _sfliderState.config = config;
+  _sfliderState.obj    = obj;
+
+  _sfliderState.slidesCount =
+    $(_sfliderState.obj).children(".sflider-slides").children(".sflider-slide").length;
+
+  fixHeight(
+    $(_sfliderState.obj),
+    _sfliderState.config.height
+  );
+  $(_sfliderState.obj).css(
+    "width",
+    (_sfliderState.slidesCount*100)+"%"
+  );
+  $(_sfliderState.obj).children(".sflider-slides").children(".sflider-slide").css(
+    "width",
+    (100/_sfliderState.slidesCount)+"%"
+  );
+  fixHeight(
+    $(obj).children(".control-key"),
+    _sfliderState.config.height
+  );
+  fixHeight(
+    $(obj).children(".sflider-slides").children(".sflider-slide"),
+    _sfliderState.config.height
+  );
+
+  $(_sfliderState.obj)
+    .mouseenter(function() {
+      _sfliderState.slideChangeAllowed=false;
+    })
+    .mouseleave(function() {
+      _sfliderState.slideChangeAllowed=true;
+  });
+
+  setSfliderTimer();
+}
+
+function _moveSflider(offset, force){
+  if (_sfliderState.slideChangeAllowed || force){
+    _sfliderState.currentSlideId =
+      mod(_sfliderState.currentSlideId+offset, _sfliderState.slidesCount);
+
+    $(_sfliderState.obj).children(".sflider-slides")
+      .animate({left: "-"+(_sfliderState.currentSlideId*(100/_sfliderState.slidesCount))+"%"});
+  }
+
+  setSfliderTimer();
+}
